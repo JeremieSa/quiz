96475a5a8db346626c66134de454d03cedd784bb
Jeremie SAINGRE <jeremie.saingre@etu.univ-nantes.fr>
2021-11-29 18:33:06 +0100

    Put await in front of async functions in Quiz.test

--- test/Quiz.test.js
+++ test/Quiz.test.js
@@ -3,15 +3,15 @@ import * as sauvegarde from '../src/modele/Sauvegardes.js';
 
 describe('Modele QuestionParser', () => {
     test('Get the right total of questions', () => {
-        expect(questionParser.getNbQuestions()).toBe(4);
+        expect(await questionParser.getNbQuestions()).toBe(4);
     }),
 
     test('For an index, get the right question', () => {
-        expect(questionParser.getQuestion(0)).toBe("Vitesse max libellule ? (km/h)");
+        expect(await questionParser.getQuestion(0)).toBe("Vitesse max libellule ? (km/h)");
     }),
 
     test('Get the right array of questions', () => {
-        expect(questionParser.getAllQuestions().toBe(["Vitesse max libellule ? (km/h)",
+        expect(await questionParser.getAllQuestions().toBe(["Vitesse max libellule ? (km/h)",
                                                     "Quel est le nombre d'ailes des libellule ?", "Le type des yeux des libellules ?",
                                                     "Les libellules ont-elles 3 yeux ? (Oui / Non)"]));
     });
