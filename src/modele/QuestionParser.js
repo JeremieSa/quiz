7cf805e782b5e65e704af6af6e325c7cabb031f0
Jeremie SAINGRE <jeremie.saingre@etu.univ-nantes.fr>
2021-11-29 17:05:46 +0100

    Added two methods in QuestionParser.js to get all questions and answers

--- src/modele/QuestionParser.js
+++ src/modele/QuestionParser.js
@@ -5,25 +5,37 @@ export class QuestionParser {
         return response.json();
     }
 
+    async getNbQuestions() {
+        let json = await this.getJson();
+        return json.questionArray.question.length;
+    }
+
+    // QUESTION
     async getQuestion(index) {
         let json = await this.getJson();
         return json.questionArray.question[index];
     }
 
+    async getAllQuestions() {
+        let json = await this.getJson();
+        return json.questionArray.question;
+    }
+
+    // ANSWER
     async getAnswer(index) {
         let json = await this.getJson();
         return json.questionArray.answer[index];
     }
 
-    async getNbQuestions() {
+    async getAllAnswers() {
         let json = await this.getJson();
-        return json.questionArray.question.length;
+        return json.questionArray.answer;
     }
 }
 
 // TEST
 /*
 let testParser = new QuestionParser();
-let premiereQuestion = await testParser.getQuestion(0);
+let premiereQuestion = await testParser.getAllAnswers();
 console.log(premiereQuestion);
 */
