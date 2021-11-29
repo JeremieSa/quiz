ae08ad4ff43f10bf9ce79ef0b7ab3dcd64066118
ArthurJeannot <arthur.jeannot18@gmail.com>
2021-11-29 19:31:15 +0100

    Affichage du quiz

--- src/controleur/Controleur.js
+++ src/controleur/Controleur.js
@@ -1,15 +1,59 @@
 // Gestion des questions en utilisant la classe "Question" et le modele associe
 
-import { header, tempsRestant, question } from '../vue/AffichageQuiz';
+import * as AffichageQuiz from '../vue/AffichageQuiz.js';
+import * as QuestionParser from '../modele/QuestionParser.js';
+
+let score = 0;
+let currentQuestion = 0;
+let tempsMax = 20;
+let tpsRest, timer;
+
+window.addEventListener('DOMContentLoaded', setup);
 
 
-function quiz() {
-    header;
-    tempsRestant;
-    //document.getElementById('cmpt').innerText = "Test";
-    question;
+async function setup() {
+    AffichageQuiz.header(tempsMax);
+    tpsRest = tempsMax;
+    await quiz();
 }
 
-function setup() {
-    quiz();
+async function quiz() {
+    let nbQ = await QuestionParser.getNbQuestions();
+    if(currentQuestion < await QuestionParser.getNbQuestions()){
+        let quest = await QuestionParser.getQuestion(currentQuestion);
+        AffichageQuiz.question(quest);
+        timer = window.setInterval(setTimer, 1000);
+    }
+    else{
+        AffichageQuiz.finQuiz(score, nbQ);
+    }
+}
+
+
+export async function processAnswer(answer){
+    let rightAnswer = await QuestionParser.getAnswer(currentQuestion);
+    if(answer.trim().toLowerCase() == rightAnswer.trim().toLowerCase()) {
+        console.log("Right answer !");
+        score++;
+        await goToNextQuestion();
+    } else {
+        console.log("Nope !");
+        document.getElementById('answerInput').value = "";
+    }
+}
+
+async function goToNextQuestion() {
+    window.clearInterval(timer);
+    tpsRest = tempsMax;
+    AffichageQuiz.makeLastQuestionInactive();
+    currentQuestion++;
+    await quiz();
+}
+
+async function setTimer() {
+    tpsRest--;
+    AffichageQuiz.majTimer(tpsRest);
+    if (tpsRest <= 0){
+        await goToNextQuestion();
+    }
 }
--- src/vue/AffichageQuiz.js
+++ src/vue/AffichageQuiz.js
@@ -1,58 +1,66 @@
-function header() {
+import { processAnswer } from '../controleur/Controleur.js';
+
+function header(timer) {
     // Affiche le titre du quizz, le temps restant et score (question répondu sur question total)
     const startingText = document.createElement('h1');
     startingText.appendChild(document.createTextNode('Bienvenue dans le quizz des Libellules !'));
     document.body.appendChild(startingText);
-}
 
-function question() {
-    // Affiche une question (fait appel au controleur pour savoir quoi afficher)
-
-    // Numero de question
-    // Question
-
-    // Espace pour ecrire la reponse
-
-    // Bouton submit (fait appel au controleur pour savoir si la reponse est bonne)
-}
-
-function passageProchaineQuestion() {
-    // (appel constructeur)
-
-    // Question precedente :
-    // Desactive l'input et supprime le bouton submit de la derniere question
-    // Affiche vraie ou faux (si faux : affiche la reonse attendue)
-
-    // Nouvelle question :
-    // Fait appel a question();
-}
-
-function finQuiz() {
-    // Message de fin de quiz avec le score et un text du style "Excellent !" ou "Vous etes une merde" selon le score
-}
-
-function tempsRestant() {
+    
     // Affichage du temps restant
     const time = document.createElement('p');
-    time.appendChild(document.createTextNode('Temps restant pour la question: '));
+    time.appendChild(document.createTextNode('Temps restant pour répondre a la question actuel: '));
     let compteur = document.createElement('span');
     compteur.setAttribute('id', 'cmpt');
+    compteur.innerText = timer;
     time.appendChild(compteur);
     document.body.appendChild(time);
 }
 
-/* Doit fonctionner etre dans Controlleur
+function question(question) {
+    // Affiche une question (fait appel au controleur pour savoir quoi afficher)
+    const quest = document.createElement('p');
+    // Numero de question
+    // Question
+    quest.appendChild(document.createTextNode(question));
+    document.body.appendChild(quest);
+    // Espace pour ecrire la reponse
+    const answerInput = document.createElement('input');
+    answerInput.setAttribute('type', 'text');
+    answerInput.setAttribute('id', 'answerInput');
+    answerInput.setAttribute('placeholder', 'Answer Input');
+    document.body.appendChild(answerInput);
 
-function quiz() {
-    header();
-    tempsRestant();
-    //document.getElementById('cmpt').innerText = "Test";
-    question();
+    const submitAnswer = document.createElement('button');
+    submitAnswer.setAttribute('id', 'submitAnswer');
+    submitAnswer.appendChild(document.createTextNode('Submit answer'));
+    submitAnswer.addEventListener('click', () => processAnswer(document.getElementById('answerInput').value));
+    document.body.appendChild(submitAnswer);
+
+
+    // Bouton submit (fait appel au controleur pour savoir si la reponse est bonne)
 }
 
-function setup() {
-    quiz();
+function makeLastQuestionInactive() {
+    // (appel constructeur)
+    //window.clearTimeout(idTime);
+    // Question precedente :
+    // Desactive l'input et supprime le bouton submit de la derniere question
+    document.getElementById('answerInput').disabled = true;
+    document.getElementById('answerInput').setAttribute('id', '');
+    document.getElementById('submitAnswer').remove();
 }
-*/
 
-window.addEventListener('DOMContentLoaded', setup);
+function finQuiz(score, nbQ) {
+    // Message de fin de quiz avec le score et un text du style "Excellent !" ou "Vous etes une merde" selon le score
+    const fin = document.createElement('p');
+    fin.appendChild(document.createTextNode('Le quiz est fini! Vous avez obtenu '+ score +' bonnes réponses sur '+ nbQ));
+    document.body.appendChild(fin);
+}
+
+
+export {header, question, finQuiz, makeLastQuestionInactive}
+
+export function majTimer(tpsRest) {
+   document.getElementById('cmpt').innerText = tpsRest;
+}
